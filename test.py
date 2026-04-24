d603c0eadcff3cb814e138f18fc19d0b83054dee
Poddar2710 <arpitakumari2303@gmail.com>
2026-04-24 13:13:07 +0530

    Update test.py
    Updated API key to use a placeholder for security.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="AIzaSyBcfDVKb6gDm3MeLj-Of2b-gPd6zn4xnWg")
+client = genai.Client(api_key="your_api_key")
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", contents="Explain how AI works in a few words"
